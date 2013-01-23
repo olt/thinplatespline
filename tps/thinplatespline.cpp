24dd4ae16d78805e7d0098a719da6d792e890c45
Oliver Tonnhofer <olt@bogosoft.com>
2013-01-23 10:08:09 +0100

    comment out print statement on tps errors

--- tps/thinplatespline.cpp
+++ tps/thinplatespline.cpp
@@ -2,12 +2,12 @@
  * $Id: thinplatespline.cpp 15547 2008-10-17 17:45:03Z rouault $
  *
  * Project:  GDAL Warp API
- * Purpose:  Implemenentation of 2D Thin Plate Spline transformer. 
+ * Purpose:  Implemenentation of 2D Thin Plate Spline transformer.
  * Author:   VIZRT Development Team.
  *
- * This code was provided by Gilad Ronnen (gro at visrt dot com) with 
+ * This code was provided by Gilad Ronnen (gro at visrt dot com) with
  * permission to reuse under the following license.
- * 
+ *
  ******************************************************************************
  * Copyright (c) 2004, VIZRT Inc.
  *
@@ -63,7 +63,7 @@ void VizGeorefSpline2D::grow_points()
 {
     int new_max = _max_nof_points*2 + 2 + 3;
     int i;
-    
+
     if( _max_nof_points == 0 )
     {
         x = (double *) malloc( sizeof(double) * new_max );
@@ -86,9 +86,9 @@ void VizGeorefSpline2D::grow_points()
         index = (int *) realloc( index, sizeof(int) * new_max );
         for( i = 0; i < VIZGEOREF_MAX_VARS; i++ )
         {
-            rhs[i] = (double *) 
+            rhs[i] = (double *)
                 realloc( rhs[i], sizeof(double) * new_max );
-            coef[i] = (double *) 
+            coef[i] = (double *)
                 realloc( coef[i], sizeof(double) * new_max );
         }
     }
@@ -172,14 +172,14 @@ int VizGeorefSpline2D::solve(void)
 {
     int r, c, v;
     int p;
-	
+
     //	No points at all
     if ( _nof_points < 1 )
     {
         type = VIZ_GEOREF_SPLINE_ZERO_POINTS;
         return(0);
     }
-	
+
     // Only one point
     if ( _nof_points == 1 )
     {
@@ -190,33 +190,33 @@ int VizGeorefSpline2D::solve(void)
     if ( _nof_points == 2 )
     {
         _dx = x[1] - x[0];
-        _dy = y[1] - y[0];	 
+        _dy = y[1] - y[0];
         double fact = 1.0 / ( _dx * _dx + _dy * _dy );
         _dx *= fact;
         _dy *= fact;
-		
+
         type = VIZ_GEOREF_SPLINE_TWO_POINTS;
         return(2);
     }
-	
+
     // More than 2 points - first we have to check if it is 1D or 2D case
-		
+
     double xmax = x[0], xmin = x[0], ymax = y[0], ymin = y[0];
     double delx, dely;
     double xx, yy;
     double sumx = 0.0f, sumy= 0.0f, sumx2 = 0.0f, sumy2 = 0.0f, sumxy = 0.0f;
     double SSxx, SSyy, SSxy;
-	
+
     for ( p = 0; p < _nof_points; p++ )
     {
         xx = x[p];
         yy = y[p];
-		
+
         xmax = MAX( xmax, xx );
         xmin = MIN( xmin, xx );
         ymax = MAX( ymax, yy );
         ymin = MIN( ymin, yy );
-		
+
         sumx  += xx;
         sumx2 += xx * xx;
         sumy  += yy;
@@ -225,24 +225,24 @@ int VizGeorefSpline2D::solve(void)
     }
     delx = xmax - xmin;
     dely = ymax - ymin;
-	
+
     SSxx = sumx2 - sumx * sumx / _nof_points;
     SSyy = sumy2 - sumy * sumy / _nof_points;
     SSxy = sumxy - sumx * sumy / _nof_points;
-	
-    if ( delx < 0.001 * dely || dely < 0.001 * delx || 
+
+    if ( delx < 0.001 * dely || dely < 0.001 * delx ||
          fabs ( SSxy * SSxy / ( SSxx * SSyy ) ) > 0.99 )
     {
         int p1;
-		
+
         type = VIZ_GEOREF_SPLINE_ONE_DIMENSIONAL;
-		
+
         _dx = _nof_points * sumx2 - sumx * sumx;
         _dy = _nof_points * sumy2 - sumy * sumy;
         double fact = 1.0 / sqrt( _dx * _dx + _dy * _dy );
         _dx *= fact;
         _dy *= fact;
-		
+
         for ( p = 0; p < _nof_points; p++ )
         {
             double dxp = x[p] - x[0];
@@ -250,7 +250,7 @@ int VizGeorefSpline2D::solve(void)
             u[p] = _dx * dxp + _dy * dyp;
             unused[p] = 1;
         }
-		
+
         for ( p = 0; p < _nof_points; p++ )
         {
             int min_index = -1;
@@ -269,38 +269,38 @@ int VizGeorefSpline2D::solve(void)
             index[p] = min_index;
             unused[min_index] = 0;
         }
-		
+
         return(3);
     }
-	
+
     type = VIZ_GEOREF_SPLINE_FULL;
     // Make the necessary memory allocations
     if ( _AA )
         free(_AA);
     if ( _Ainv )
         free(_Ainv);
-	
+
     _nof_eqs = _nof_points + 3;
-	
+
     _AA = ( double * )calloc( _nof_eqs * _nof_eqs, sizeof( double ) );
     _Ainv = ( double * )calloc( _nof_eqs * _nof_eqs, sizeof( double ) );
-	
+
     // Calc the values of the matrix A
     for ( r = 0; r < 3; r++ )
         for ( c = 0; c < 3; c++ )
             A(r,c) = 0.0;
-		
+
     for ( c = 0; c < _nof_points; c++ )
     {
         A(0,c+3) = 1.0;
         A(1,c+3) = x[c];
         A(2,c+3) = y[c];
-			
+
         A(c+3,0) = 1.0;
         A(c+3,1) = x[c];
         A(c+3,2) = y[c];
     }
-		
+
     for ( r = 0; r < _nof_points; r++ )
         for ( c = r; c < _nof_points; c++ )
         {
@@ -308,27 +308,27 @@ int VizGeorefSpline2D::solve(void)
             if ( r != c )
                 A(c+3,r+3 ) = A(r+3,c+3);
         }
-			
+
 #if VIZ_GEOREF_SPLINE_DEBUG
-			
+
     for ( r = 0; r < _nof_eqs; r++ )
     {
         for ( c = 0; c < _nof_eqs; c++ )
             fprintf(stderr, "%f", A(r,c));
         fprintf(stderr, "\n");
     }
-			
+
 #endif
-			
+
     // Invert the matrix
     int status = matrixInvert( _nof_eqs, _AA, _Ainv );
-			
+
     if ( !status )
     {
-        fprintf(stderr, " There is a problem to invert the interpolation matrix\n");
+        // fprintf(stderr, " There is a problem to invert the interpolation matrix\n");
         return 0;
     }
-			
+
     // calc the coefs
     for ( v = 0; v < _nof_vars; v++ )
         for ( r = 0; r < _nof_eqs; r++ )
@@ -337,7 +337,7 @@ int VizGeorefSpline2D::solve(void)
             for ( c = 0; c < _nof_eqs; c++ )
                 coef[v][r] += Ainv(r,c) * rhs[v][c];
         }
-				
+
     return(4);
 }
 
@@ -347,7 +347,7 @@ int VizGeorefSpline2D::get_point( const double Px, const double Py, double *vars
 	double tmp, Pu;
 	double fact;
 	int leftP=0, rightP=0, found = 0;
-	
+
 	switch ( type )
 	{
 	case VIZ_GEOREF_SPLINE_ZERO_POINTS :
@@ -380,12 +380,12 @@ int VizGeorefSpline2D::get_point( const double Px, const double Py, double *vars
 			for ( r = 1; !found && r < _nof_points; r++ )
 			{
 				leftP = index[r-1];
-				rightP = index[r];					
+				rightP = index[r];
 				if ( Pu >= u[leftP] && Pu <= u[rightP] )
 					found = 1;
 			}
 		}
-		
+
 		fact = ( Pu - u[leftP] ) / ( u[rightP] - u[leftP] );
 		for ( v = 0; v < _nof_vars; v++ )
 			vars[v] = ( 1.0 - fact ) * rhs[v][leftP+3] +
@@ -394,7 +394,7 @@ int VizGeorefSpline2D::get_point( const double Px, const double Py, double *vars
 	case VIZ_GEOREF_SPLINE_FULL :
 		for ( v = 0; v < _nof_vars; v++ )
 			vars[v] = coef[v][0] + coef[v][1] * Px + coef[v][2] * Py;
-		
+
 		for ( r = 0; r < _nof_points; r++ )
 		{
 			tmp = base_func( Px, Py, x[r], y[r] );
@@ -428,10 +428,10 @@ double VizGeorefSpline2D::base_func( const double x1, const double y1,
 {
 	if ( ( x1 == x2 ) && (y1 == y2 ) )
 		return 0.0;
-	
+
 	double dist  = ( x2 - x1 ) * ( x2 - x1 ) + ( y2 - y1 ) * ( y2 - y1 );
-	
-	return dist * log( dist );	
+
+	return dist * log( dist );
 }
 
 int matrixInvert( int N, double input[], double output[] )
@@ -441,15 +441,15 @@ int matrixInvert( int N, double input[], double output[] )
     // input matrix, returned as output, also of size N-squared.  The Gauss-
     // Jordan Elimination method is used.  (Adapted from a BASIC routine in
     // "Basic Scientific Subroutines Vol. 1", courtesy of Scott Edwards.)
-	
+
     // Array elements 0...N-1 are for the first row, N...2N-1 are for the
     // second row, etc.
-	
+
     // We need to have a temporary array of size N x 2N.  We'll refer to the
     // "left" and "right" halves of this array.
-	
+
     int row, col;
-	
+
 #if 0
     fprintf(stderr, "Matrix Inversion input matrix (N=%d)\n", N);
     for ( row=0; row<N; row++ )
@@ -461,54 +461,54 @@ int matrixInvert( int N, double input[], double output[] )
         fprintf(stderr, "\n");
     }
 #endif
-	
+
     int tempSize = 2 * N * N;
     double* temp = (double*) new double[ tempSize ];
     double ftemp;
-	
+
     if (temp == 0) {
-		
+
         fprintf(stderr, "matrixInvert(): ERROR - memory allocation failed.\n");
         return false;
     }
-	
+
     // First create a double-width matrix with the input array on the left
     // and the identity matrix on the right.
-	
+
     for ( row=0; row<N; row++ )
     {
         for ( col=0; col<N; col++ )
         {
             // Our index into the temp array is X2 because it's twice as wide
             // as the input matrix.
-			
+
             temp[ 2*row*N + col ] = input[ row*N+col ];	// left = input matrix
             temp[ 2*row*N + col + N ] = 0.0f;			// right = 0
         }
         temp[ 2*row*N + row + N ] = 1.0f;		// 1 on the diagonal of RHS
     }
-	
+
     // Now perform row-oriented operations to convert the left hand side
     // of temp to the identity matrix.  The inverse of input will then be
     // on the right.
-	
+
     int max;
     int k=0;
     for (k = 0; k < N; k++)
     {
         if (k+1 < N)	// if not on the last row
-        {              
+        {
             max = k;
             for (row = k+1; row < N; row++) // find the maximum element
-            {  
+            {
                 if (fabs( temp[row*2*N + k] ) > fabs( temp[max*2*N + k] ))
                 {
                     max = row;
                 }
             }
-			
+
             if (max != k)	// swap all the elements in the two rows
-            {        
+            {
                 for (col=k; col<2*N; col++)
                 {
                     ftemp = temp[k*2*N + col];
@@ -517,34 +517,34 @@ int matrixInvert( int N, double input[], double output[] )
                 }
             }
         }
-		
+
         ftemp = temp[ k*2*N + k ];
         if ( ftemp == 0.0f ) // matrix cannot be inverted
         {
             delete[] temp;
             return false;
         }
-		
+
         for ( col=k; col<2*N; col++ )
         {
             temp[ k*2*N + col ] /= ftemp;
         }
-		
+
         for ( row=0; row<N; row++ )
         {
             if ( row != k )
             {
                 ftemp = temp[ row*2*N + k ];
-                for ( col=k; col<2*N; col++ ) 
+                for ( col=k; col<2*N; col++ )
                 {
                     temp[ row*2*N + col ] -= ftemp * temp[ k*2*N + col ];
                 }
             }
         }
     }
-	
+
     // Retrieve inverse from the right side of temp
-	
+
     for (row = 0; row < N; row++)
     {
         for (col = 0; col < N; col++)
@@ -552,7 +552,7 @@ int matrixInvert( int N, double input[], double output[] )
             output[row*N + col] = temp[row*2*N + col + N ];
         }
     }
-	
+
 #if 0
     fprintf(stderr, "Matrix Inversion result matrix:\n");
     for ( row=0; row<N; row++ )
@@ -564,7 +564,7 @@ int matrixInvert( int N, double input[], double output[] )
         fprintf(stderr, "\n");
     }
 #endif
-	
+
     delete [] temp;       // free memory
     return true;
 }
